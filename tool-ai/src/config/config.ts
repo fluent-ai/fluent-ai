e6702463e4dae3ef471578d26d9fa455d45d4811
Deftool66 <deftool@gmx.net>
2023-05-08 16:49:26 +0200

    started the firebase config

---
+++ tool-ai/src/config/config.ts
@@ -0,0 +1,15 @@
+// Contains Global Configurations
+const config = {
+  firebase: {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  },
+};
+
+export default config;
