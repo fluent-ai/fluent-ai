fa8770f08782b3e21449cefddff50425fcef20de
ainaperez <aina.perez.1996@gmail.com>
2023-05-09 19:12:11 +0200

    feat: create Avatar component

---
+++ apps/toolai/src/components/UI/AvatarComponent/AvatarComponent.tsx
@@ -0,0 +1,20 @@
+import * as Avatar from '@radix-ui/react-avatar';
+import styles from './AvatarComponent.module.css';
+
+interface AvatarProps {
+  initials: string;
+  name?: string
+  url?: string;
+  alt?: string
+}
+const AvatarComponent = (props: AvatarProps) => (
+  <Avatar.Root className={styles.AvatarRoot}>
+    <Avatar.Image
+    className={styles.AvatarImage}
+    src={props.url}
+    alt={props.alt} />
+    <Avatar.Fallback className={styles.AvatarFallback}>{props.initials}</Avatar.Fallback>
+  </Avatar.Root>
+);
+
+export default AvatarComponent;
