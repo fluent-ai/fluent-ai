bab4da3552add8392628bcd60f7cdad6c2fb9f40
ainaperez <aina.perez.1996@gmail.com>
2023-05-09 19:13:36 +0200

    feat: create templateNode component

---
+++ apps/toolai/src/components/Nodes/TemplateNode/TemplateNode.spec.tsx
@@ -0,0 +1,34 @@
+import React, { memo, useContext } from 'react';
+import { Handle, Position } from 'reactflow';
+import NodeDialogComponent from '../UI/NodeDialogComponent';
+import './CustomNodesStyles.css';
+import * as Dialog from '@radix-ui/react-dialog';
+import Context from '../context/context';
+
+interface MemoProps {
+  isConnectable: boolean;
+  data: any
+}
+export default memo (({data, isConnectable}: MemoProps) => {
+  const {setIsDialogOpen} = useContext(Context);
+  return (
+    <>
+      <Handle
+        type="target"
+        position={Position.Top}
+        style={{ background: '#555' }}
+        onConnect={(params) => console.log('handle onConnect', params)}
+        isConnectable={isConnectable}
+      />
+      <div onClick={()=>setIsDialogOpen(true)}>{data.label}</div>
+
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="b"
+        style={{top: 'auto', background: '#555' }}
+        isConnectable={isConnectable}
+      />
+    </>
+  );
+  });
