572272b120817a62dc9d839a7e6e73773e00b3a4
theronburger <dev@theronburger.com>
2023-05-10 16:59:17 +0200

    test(flow-runner): add tests for input node

--- libs/flow-runner/src/lib/nodeMethods/input.spec.ts
+++ libs/flow-runner/src/lib/nodeMethods/input.spec.ts
@@ -1,28 +1,26 @@
 import { input } from './input';
 
-describe('nodeMethods', () => {
-  describe('input', () => {
-    it('should pass a string from data to msg', async () => {
-      const inputData = {
-        input: 'Hello, world!',
-      };
-      const mockMsg = {};
+describe('FlowRunner nodeMethods - input', () => {
+  it('should pass a string from data to msg', async () => {
+    const inputData = {
+      input: 'Hello, world!',
+    };
+    const mockMsg = {};
 
-      const result = await input(mockMsg, inputData);
+    const result = await input(mockMsg, inputData);
 
-      expect(result?.['payload']).toBe('Hello, world!');
-    });
+    expect(result?.['payload']).toBe('Hello, world!');
+  });
 
-    it('should reject when data.input is not a string', async () => {
-      const inputData = {
-        input: 123,
-      };
-      const mockMsg = {};
+  it('should reject when data.input is not a string', async () => {
+    const inputData = {
+      input: 123,
+    };
+    const mockMsg = {};
 
-      // @ts-ignore
-      await expect(input(mockMsg, inputData)).rejects.toThrow(
-        'data.input is not a string'
-      );
-    });
+    // @ts-expect-error - testing invalid input
+    await expect(input(mockMsg, inputData)).rejects.toThrow(
+      'data.input is not a string'
+    );
   });
 });
