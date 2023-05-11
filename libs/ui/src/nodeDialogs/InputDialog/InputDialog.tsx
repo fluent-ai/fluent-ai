d67efb5a5ec83306e7f643e9e3024fb50a61fd4c
ainaperez <aina.perez.1996@gmail.com>
2023-05-11 19:15:39 +0200

    feat(NodeDialog): Add input dialog structure

--- libs/ui/src/nodeDialogs/InputDialog/InputDialog.tsx
+++ libs/ui/src/nodeDialogs/InputDialog/InputDialog.tsx
@@ -1,12 +1,27 @@
 /* eslint-disable-next-line */
-export interface InputDialogProps {}
+import { InnerDialogStructure } from "../../lib/InnerDialogStructure/InnerDialogStructure";
+import { NodeDialogProps } from "../../types";
+import { handleChange } from '../functions';
 
-function InputDialog(props: InputDialogProps) {
+
+
+function InputDialog(props: NodeDialogProps) {
   return (
-    <div>
-      <h3>Text Input</h3>
-      <textarea placeholder="input your text" rows={10} cols={100} />
-    </div>
+    <InnerDialogStructure
+    title="Text Input"
+    description="text input description">
+      <textarea
+        className="border-2 border-gray-light border-solid rounded-md w-full"
+        placeholder="input your text"
+        rows={10}
+        cols={100}
+        onChange={(event) => handleChange(
+          props.nodes,
+          props.setNodes,
+          props.activeNodeId,
+          event.target.value,
+          'input')} />
+    </InnerDialogStructure>
   );
 }
 
