daffaaabf95ae745c06bb2a56dfcd36d1101f131
juice1000 <julienlook@gmx.de>
2023-05-11 11:43:27 +0200

    types.d for ui lib

--- libs/ui/src/lib/FormFieldComponent/FormFieldComponent.tsx
+++ libs/ui/src/lib/FormFieldComponent/FormFieldComponent.tsx
@@ -1,30 +1,18 @@
 /* eslint-disable-next-line */
 import * as Form from '@radix-ui/react-form';
 import { SetStateAction, Dispatch } from 'react';
-export interface Validation {
-  match: "badInput" | "patternMismatch" | "rangeOverflow" | "rangeUnderflow" | "stepMismatch" | "tooLong" | "tooShort" | "typeMismatch" | "valid" | "valueMissing" | Form.CustomMatcher | undefined,
-  message: string
-}
+import { FormFieldProps, Validation } from '../../types';
 
-export interface FormFieldProps {
-  label: string
-  validations?: Validation[],
-  type:string,
-  required?: boolean,
-  value?: any,
-  onChange: any,
-  placeholder?:string
-}
-
-function FormFieldComponent (props: FormFieldProps) {
+function FormFieldComponent(props: FormFieldProps) {
   return (
     <Form.Field className="grid mb-[10px]" name="password">
       <div className="flex items-baseline justify-between">
         <Form.Label className="">{props.label}</Form.Label>
-        {props.validations?.map((validation:Validation) => <Form.Message className="" match={validation.match}>
-          {validation.message}
-        </Form.Message>
-        )}
+        {props.validations?.map((validation: Validation) => (
+          <Form.Message className="" match={validation.match}>
+            {validation.message}
+          </Form.Message>
+        ))}
       </div>
       <Form.Control asChild>
         <input
@@ -32,7 +20,7 @@ function FormFieldComponent (props: FormFieldProps) {
           type={props.type}
           required={props.required}
           value={props.value}
-          onChange={(e)=>props.onChange(e)}
+          onChange={(e) => props.onChange(e)}
           placeholder={props.placeholder}
         />
       </Form.Control>
@@ -40,4 +28,4 @@ function FormFieldComponent (props: FormFieldProps) {
   );
 }
 
-export {FormFieldComponent};
+export { FormFieldComponent };
--- libs/ui/src/lib/TooltipComponent/TooltipComponent.tsx
+++ libs/ui/src/lib/TooltipComponent/TooltipComponent.tsx
@@ -1,41 +1,36 @@
 import React from 'react';
 import * as Tooltip from '@radix-ui/react-tooltip';
-
-
-interface TooltipProps {
-  buttonContent?: JSX.Element | string;
-  text: string
-}
+import { TooltipProps } from '../../types';
 
 const TooltipComponent = (props: TooltipProps) => {
   return (
     <Tooltip.Provider>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild>
-        <button
-          aria-label="iconbutton"
-          className="sidebar-icon h-9
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <button
+            aria-label="iconbutton"
+            className="sidebar-icon h-9
           w-9 inline-flex
           items-center
           justify-center
          text-black bg-white rounded-full sidebar-icon
          hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-offset-2"
-        >
-          {props.buttonContent}
-        </button>
-      </Tooltip.Trigger>
-      <Tooltip.Portal>
-        <Tooltip.Content
-          className="border border-gray-200 shadow-md p-2 text-sm text-black bg-white rounded-lg select-none z-20"
-          sideOffset={5}
-        >
-          {props.text}
-          <Tooltip.Arrow className="text-white" />
-        </Tooltip.Content>
-      </Tooltip.Portal>
-    </Tooltip.Root>
-  </Tooltip.Provider>
+          >
+            {props.buttonContent}
+          </button>
+        </Tooltip.Trigger>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className="border border-gray-200 shadow-md p-2 text-sm text-black bg-white rounded-lg select-none z-20"
+            sideOffset={5}
+          >
+            {props.text}
+            <Tooltip.Arrow className="text-white" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
 
-export {TooltipComponent};
+export { TooltipComponent };
