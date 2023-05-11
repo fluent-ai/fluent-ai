fb1cb9dad44ec2f5ef690b25994bf02faccfe0b3
theronburger <dev@theronburger.com>
2023-05-11 12:18:59 +0200

    feat: add state provider to app main.tsx

--- apps/toolai/src/main.tsx
+++ apps/toolai/src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './firebase-init';
 import App from './app/app';
+import { FlowStateProvider } from '@tool-ai/state';
 // import * as auth from '@libs/auth';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <FlowStateProvider>
+        <App />
+      </FlowStateProvider>
+    </StrictMode>
 );
