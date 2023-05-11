1b27076ce3b235827dfc58112ff3e5604db4017a
Deftool66 <deftool@gmx.net>
2023-05-11 18:26:41 +0200

    "save the google user in the firestore dB"

--- libs/auth/src/lib/GoogleLogin/GoogleLogin.tsx
+++ libs/auth/src/lib/GoogleLogin/GoogleLogin.tsx
@@ -1,5 +1,8 @@
 import styles from './GoogleLogin.module.css';
 import { signInWithPopup, GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import * as firestoreService from '@libs/firestore-service';
+import { User } from '@tool-ai/ui';
 // import provider from 'firebase/auth';
 
 /* eslint-disable-next-line */
@@ -10,6 +13,7 @@ const auth = getAuth();
 export function GoogleLogin() {
   // creating the sign in with google react component
   const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
   provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
   const signInWithGoogle = () => {
     console.log('sign in with google');
@@ -18,9 +22,20 @@ export function GoogleLogin() {
       .then((result) => {
         console.log('result', result);
         const credential = GoogleAuthProvider.credentialFromResult(result);
+        console.log('userCredential', credential);
         const token = credential?.accessToken;
         const user = result.user;
-        console.log('user', user);
+        if (user.displayName && user.email && user.photoURL) {
+          const newUser: User = {
+            id: user.uid,
+            email: user.email,
+            name: user.displayName,
+            initials: user.displayName?.slice(0, 2).toUpperCase(),
+            profileImg: user.photoURL,
+          };
+          firestoreService.writeToDB('users', newUser);
+          navigate('/');
+        }
       })
       .catch((error) => {
         const errorCode = error.code;
