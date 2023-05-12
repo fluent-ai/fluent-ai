d10d3c653912fb7d20dc6b83ba61962c9cb0a3b4
ainaperez <aina.perez.1996@gmail.com>
2023-05-12 10:55:49 +0200

    feat(dashboard): add run button

--- apps/toolai/src/components/Pages/Dashboard/Dashboard.tsx
+++ apps/toolai/src/components/Pages/Dashboard/Dashboard.tsx
@@ -6,8 +6,8 @@ import {
   addEdge,
   useNodesState,
   useEdgesState,
-  Node
-
+  Node,
+  Edge
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import NodeSideBar from '../../Navigation/NodeSideBar/NodeSideBar';
@@ -15,6 +15,7 @@ import FlowTabs from '../../Navigation/FlowTabs/FlowTabs';
 import TemplateNode from '../../Nodes/TemplateNode/TemplateNode';
 //import { NodeWrapperComponent } from '@tool-ai/ui';
 import Header from '../../Navigation/Header/Header';
+import { ButtonComponent } from '@tool-ai/ui';
 
 const nodeTypes = {
   textInput: TemplateNode,
@@ -24,19 +25,160 @@ const nodeTypes = {
   preview: TemplateNode,
 };
 
-const initialNodes:Node[] = [
+const initialNodes = [
   {
     id: '1',
-    type: 'textInput',
-    data: { label: 'textInput' },
-    position: { x: 250, y: 5 },
+    type: 'input',
+    data: {
+      label: 'Text input',
+    },
+    props: {
+      input: `{
+          "name" : "Mr Wiggles",
+          "color" : "pink",
+          "number" : 3,
+          "balloons" : true
+        }
+        `,
+    },
+    msg: {
+      payload: `{
+          "name" : "Mr Wiggles",
+          "color" : "pink",
+          "number" : 3,
+          "balloons" : true
+        }
+        `,
+    },
+    position: { x: 0, y: 50 },
   },
   {
     id: '2',
-    type: 'template',
-    data: { label: 'template' },
+    type: 'json',
+    data: {
+      label: 'JSON',
+    },
+    msg :{
+      name: "Mr Wiggles",
+      color: "pink",
+      number: 3,
+      balloons: true
+  },
     position: { x: 300, y: 50 },
+  },
+  {
+    id: '3',
+    type: 'template',
+    data: {
+      label: 'Template',
+    },
+    props: {
+      template: `Hello {{msg.payload.name}}!
+        Here! have {{msg.payload.number}} {{msg.payload.color}} balloons.`,
+    },
+    msg: {
+      payload : "Hello Mr Wiggles!\n        Here! have 3 pink balloons."
+    },
+    position: { x: 300, y: 50 },
+  },
+  {
+    id: '4',
+    type: 'preview',
+    data: { label: 'Preview' },
+    msg: {
+      payload : "Hello Mr Wiggles!\n        Here! have 3 pink balloons."
+    },
+    position: { x: 300, y: 50 },
+  },
+  {
+    id: '5',
+    type: 'output',
+    data: { label: 'Output' },
+    msg: {
+      payload : "Hello Mr Wiggles!\n        Here! have 3 pink balloons."
+    },
+    position: { x: 650, y: 25 },
+  },
+  {
+    id: '6',
+    type: 'userFunction',
+    data: {
+      label: 'Function',
+    },
+    props: {
+      userFunction: 'msg.payload.number = msg.payload.number * 2; return msg',
+    },
+    msg :{
+      name: "Mr Wiggles",
+      color: "pink",
+      number: 6,
+      balloons: true
+  },
+    position: { x: 650, y: 25 },
+  },
+  {
+    id: '7',
+    type: 'output',
+    data: { label: 'Output' },
+    msg :{
+      name: "Mr Wiggles",
+      color: "pink",
+      number: 6,
+      balloons: true
+  },
+    position: { x: 650, y: 25 },
+  },
+  {
+    id: '8',
+    type: 'template',
+    data: {
+      label: 'Template',
+    },
+    props: {
+      template: `Im a redundant template!`,
+    },
+    msg: {
+      payload: `Im a redundant template!`,
+    },
+    position: { x: 300, y: 50 },
+  },
+];
 
+const initialEdges = [
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2',
+  },
+  {
+    id: 'e2-3',
+    source: '2',
+    target: '3',
+  },
+  {
+    id: 'e3-4',
+    source: '3',
+    target: '4',
+  },
+  {
+    id: 'e3-5',
+    source: '3',
+    target: '5',
+  },
+  {
+    id: 'e4-5',
+    source: '4',
+    target: '5',
+  },
+  {
+    id: 'e2-6',
+    source: '2',
+    target: '6',
+  },
+  {
+    id: 'e6-7',
+    source: '6',
+    target: '7',
   },
 ];
 
@@ -46,7 +188,7 @@ const getId = () => `dndnode_${id++}`;
 const Dashboard = () => {
   const reactFlowWrapper = useRef<any>(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [reactFlowInstance, setReactFlowInstance] = useState<any>(null);
 
   const onConnect = useCallback(
@@ -131,9 +273,20 @@ const Dashboard = () => {
     nodeTypes: nodeTypes,
   };
 
+  function runFlow () {console.log('Running')}
+
   return (
     <>
     <Header />
+    <div className='h-10 w-32 mt-2.5 ml-72 bg-white absolute shadow-md rounded-md z-10 text-black flex justify-between items-center'>
+    <ButtonComponent
+      buttonContent='RUN'
+      type='button'
+      classes='icons'
+      ariaLabel='run flow'
+      clickHandler={runFlow} />
+      </div>
+
       <div className="relative flex flex-col grow h-full md:flex-row">
         <ReactFlowProvider>
           <NodeSideBar />
