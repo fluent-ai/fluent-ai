02e2512ab1093a808b75d848a5da44553e8f6432
ainaperez <aina.perez.1996@gmail.com>
2023-05-13 16:41:48 +0200

    fix(config): fix webpack config for deepl

--- apps/toolai/webpack.config.js
+++ apps/toolai/webpack.config.js
@@ -1,9 +1,13 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins.push(new NodePolyfillPlugin())
+  config.resolve.fallback = {"fs":false};
   return config;
 });
