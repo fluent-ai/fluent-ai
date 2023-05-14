90b215924280339ef1767d767a9e6e87a43f595b
juice1000 <julienlook@gmx.de>
2023-05-14 17:08:13 +0200

    outsource addFlowtab to ui-interactions

--- libs/ui/src/lib/TooltipComponent/TooltipComponent.tsx
+++ libs/ui/src/lib/TooltipComponent/TooltipComponent.tsx
@@ -1,64 +1,19 @@
 import * as Tooltip from '@radix-ui/react-tooltip';
-import { flowTabActions, store, userActions } from '@tool-ai/state';
 import { TooltipProps } from '../../types';
 import { ButtonComponent } from '../ButtonComponent/ButtonComponent';
+import { addFlowTab } from '../../ui-interactions/add-flowtab';
+
 const TooltipComponent = (props: TooltipProps) => {
-  const addNewFlowTab = () => {
-    store.dispatch(userActions.setLoadingStatus('loading'));
-    const userData = store.getState().user.userData;
-    const sessionTabs = userData.flows;
-
-    const flowTabIds = sessionTabs.map((tab) => Number(tab.id.slice(-1)));
-    let newTabId = 0;
-    for (let i = 1; i <= flowTabIds.length; i++) {
-      if (i !== flowTabIds[i - 1]) {
-        newTabId = i;
-        break;
-      }
-      if (i === flowTabIds.length) {
-        newTabId = i + 1;
-      }
-    }
-
-    const newFlow = {
-      id: userData.id + '-' + newTabId, // this will count the existing tabs and assign tab number according to exisiting count
-      title: 'Flow ' + newTabId,
-      owner: true,
-      stringifiedNodes: '[]',
-      stringifiedEdges: '[]',
-      colaborators: [],
-    };
-    // store new state in redux
-    store.dispatch(userActions.updateUserFlows(newFlow));
-    store.dispatch(
-      flowTabActions.addNewFlowTab({ id: newFlow.id, nodes: [], edges: [] })
-    );
-
-    // update firestore
-    // firestoreService.updateFirestoreDocument('users', user.id, {
-    //   flows: [...user.flows],
-    // });
-    store.dispatch(userActions.setLoadingStatus('loaded'));
-  };
-
   return (
     <Tooltip.Provider>
       <Tooltip.Root>
         <Tooltip.Trigger asChild>
-          {/* <button
-          type="button"
-          className="icon"
-          aria-label="iconbutton"
-          onClick={addNewFlowTab}
-          >
-          {props.buttonContent}
-          </button> */}
           <ButtonComponent
             type="button"
             ariaLabel="iconbutton"
             buttonContent={props.buttonContent}
             classes="icon"
-            clickHandler={props.name === 'add-flow' ? addNewFlowTab : undefined}
+            clickHandler={props.name === 'add-flow' ? addFlowTab : undefined}
           />
         </Tooltip.Trigger>
         <Tooltip.Portal>
