744abae69d5432c1a6c5593c9af21c9e1d78ce48
theronburger <dev@theronburger.com>
2023-05-16 18:50:12 +0200

    feat: add description

--- libs/ui/src/nodeDialogs/TemplateDialog/TemplateDialog.tsx
+++ libs/ui/src/nodeDialogs/TemplateDialog/TemplateDialog.tsx
@@ -7,11 +7,42 @@ import { flowRunnerActions, flowRunnerSelectors } from "@tool-ai/state";
 function TemplateDialog({id}:{id:string}) {
   const dispatch = useDispatch();
   const inputs = useSelector(flowRunnerSelectors.selectInput(id));
+  const outputs = useSelector(flowRunnerSelectors.selectOutput(id));
+
+  let displayOutput = ''
+  if (typeof outputs?.nodeOutputs?.payload === 'string') {
+    displayOutput = outputs?.nodeOutputs?.payload
+  } else if (typeof outputs?.nodeOutputs?.payload === 'object') {
+    displayOutput = JSON.stringify(outputs?.nodeOutputs?.payload)
+  }
 
   return (
     <InnerDialogStructure
     title="Template"
-    description="template description" >
+    description={
+    <div>
+      Use  <b>double braces</b>, ie. {' {{ }} '} to insert data into your template.
+      <br/> The template node uses the data from the previous node 
+      <br/>Place the variable name inside the braces and it will be replaced with the value.
+      <br/>For example 
+      <pre className="text-sm p-2">
+        <code>
+          {
+            'Hello {{name}}'
+          }
+        </code>
+      </pre>
+      will be replaced with
+      <pre className="text-sm p-2">
+        <code>
+          {
+            'Hello Sally'
+          }
+        </code>
+      </pre>
+
+      </div>
+    }>
       <textarea
         className="border-2 border-gray-light border-solid rounded-md w-full"
         placeholder="Write your template here..."
@@ -29,6 +60,13 @@ function TemplateDialog({id}:{id:string}) {
             )
           )
           }}/>
+                <pre className="text-sm p-2">
+        <code>
+          {
+            displayOutput
+          }
+        </code>
+      </pre>
     </InnerDialogStructure>
   );
 }
