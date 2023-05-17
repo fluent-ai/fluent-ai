3c972b5b078c5dc53db50b9c4e8b05d5ab97e236
Deftool66 <deftool@gmx.net>
2023-05-17 11:45:52 +0200

    "getting rid of the mock user"

--- libs/ui/src/mocks.ts
+++ libs/ui/src/mocks.ts
@@ -1,19 +1,19 @@
 import { User } from './types';
-export const mockUser: User = {
-  id: 'testId',
-  email: 'test@test.com',
-  name: 'Jeff Bezos',
-  initials: 'JB',
-  flows: [
-    {
-      id: 'testId-1',
-      title: 'Flow 1',
-      stringifiedNodes: '[]',
-      stringifiedEdges: '[]',
-      owner: true,
-      colaborators: [],
-    },
-  ],
-  profileImg:
-    'https://lh3.googleusercontent.com/a/AGNmyxbYjdO34l9YBDRRFclrO7qM3cjlmxQY6F9eZ4w_Qw=s96-c',
-};
+// export const mockUser: User = {
+//   id: 'testId',
+//   email: 'test@test.com',
+//   name: 'Jeff Bezos',
+//   initials: 'JB',
+//   flows: [
+//     {
+//       id: 'testId-1',
+//       title: 'Flow 1',
+//       stringifiedNodes: '[]',
+//       stringifiedEdges: '[]',
+//       owner: true,
+//       colaborators: [],
+//     },
+//   ],
+//   profileImg:
+//     'https://lh3.googleusercontent.com/a/AGNmyxbYjdO34l9YBDRRFclrO7qM3cjlmxQY6F9eZ4w_Qw=s96-c',
+// };
