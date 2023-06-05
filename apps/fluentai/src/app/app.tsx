c70d515d1938552a1c8423ebddd2ff35aac2dc61
theronburger <dev@theronburger.com>
2023-06-05 16:06:54 +0200

    add supabase auth

--- apps/fluentai/src/app/app.tsx
+++ apps/fluentai/src/app/app.tsx
@@ -1,28 +1,41 @@
+import { createClient, Session } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import React, { useState, useRef, useCallback } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useState,useEffect } from 'react';
+import { Route,  } from 'react-router-dom';
 import Dashboard from '../components/Pages/Dashboard/Dashboard';
-import Login from '../components/Pages/Login/Login';
-import Register from '../components/Pages/Register/Register';
-// import { ProtectedRoute } from '@libs/auth';
+
+const supabase = createClient('https://rrgtmovkczotmiacaibj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZ3Rtb3ZrY3pvdG1pYWNhaWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5NjgxMzYsImV4cCI6MjAwMTU0NDEzNn0.wiV3JVN1q2-PWxBZLi1cKQ6gYRE9gyE_aQcLQXzR6mw')
+
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/register" element={<Register />}></Route>
-      <Route
-        path="/dashboard"
-        element={
-          // <ProtectedRoute>
-            <Dashboard />
-          // </ProtectedRoute>
-        }
-      ></Route>
-    </Routes>
-  );
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (
+    <Dashboard />)
+  }
 };
 
 export default App;
