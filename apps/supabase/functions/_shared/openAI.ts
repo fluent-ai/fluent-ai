2af00585bae29c3831bea13a5fa96ae48df125eb
theronburger <dev@theronburger.com>
2023-06-22 21:09:14 +0200

    add credit management to open-ai function

---
+++ apps/supabase/functions/_shared/openAI.ts
@@ -0,0 +1,36 @@
+// Defining openAi function
+export async function openAi(params = {}) {
+  const DEFAULT_PARAMS = {
+    model: 'gpt-3.5-turbo',
+    temperature: 0.7,
+    max_tokens: 1024,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  };
+
+  const params_ = {
+    ...DEFAULT_PARAMS,
+    ...params,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Deno.env.get('OPENAI_API_KEY')}`,
+    },
+    body: JSON.stringify(params_),
+  };
+
+  const response = await fetch(
+    'https://api.openai.com/v1/chat/completions',
+    requestOptions
+  );
+
+  const data = await response.json();
+
+  console.log('🤙 raw response', data);
+
+  return data;
+}
--- apps/supabase/functions/open-ai/index.ts
+++ apps/supabase/functions/open-ai/index.ts
@@ -1,110 +1,146 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 import { corsHeaders } from '../_shared/cors.ts';
+import { calculateTokenPrice } from '../_shared/openAiPricePerToken.ts';
+import { openAi } from '../_shared/openAI.ts';
 
-console.log('Making call to openAI');
-
-// Defining openAi function
-async function openAi(params = {}) {
-  const DEFAULT_PARAMS = {
-    model: 'gpt-3.5-turbo',
-    temperature: 0.7,
-    max_tokens: 1024,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  };
-
-  const params_ = {
-    ...DEFAULT_PARAMS,
-    ...params,
-  };
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${Deno.env.get('OPENAI_API_KEY')}`,
-    },
-    body: JSON.stringify(params_),
-  };
-
-  const response = await fetch(
-    'https://api.openai.com/v1/chat/completions',
-    requestOptions
-  );
-
-  const data = await response.json();
-
-  console.log('🤙 raw response', data);
-
-  return data.choices[0].message?.content;
-}
+console.log(`Function "browser-with-cors" up and running!`);
 
 serve(async (req) => {
-  //CORS
+  // This is needed if you're planning to invoke your function from a browser.
   if (req.method === 'OPTIONS') {
     return new Response('ok', { headers: corsHeaders });
   }
 
+  let params = undefined;
   try {
-    // Create a Supabase client with the Auth context of the logged in user.
-    const supabaseClient = createClient(
-      // Supabase API URL - env var exported by default.
-      Deno.env.get('SUPABASE_URL') ?? '',
-      // Supabase API ANON KEY - env var exported by default.
-      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-      // Create client with Auth context of the user that called the function.
-      // This way your row-level-security (RLS) policies are applied.
+    const body = await req.json();
+    params = body.params;
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: 'Request body must include openAI Parameters',
+        message: error,
+      }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 400,
+      }
+    );
+  }
+  console.log('🎛️ params', params);
+
+  let supabaseClient = undefined;
+  try {
+    supabaseClient = createClient(
+      Deno.env.get('SUPABASE_URL'),
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY'),
       {
         global: {
           headers: { Authorization: req.headers.get('Authorization')! },
         },
       }
     );
-    // Now we can get the session or user object
-    const {
-      data: { user },
-    } = await supabaseClient.auth.getUser();
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: 'Failed to create supabase client',
+        message: error,
+      }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 400,
+      }
+    );
+  }
 
-    console.log('🙋 user', user);
+  let user = undefined;
+  try {
+    const userData = await supabaseClient.auth.getUser();
+    user = userData?.data?.user;
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: 'Failed to get user from supabase',
+        message: error,
+      }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 400,
+      }
+    );
+  }
+  console.log('🙋 user', user);
 
-    // And we can run queries in the context of our authenticated user
-    // const { data, error } = await supabaseClient.from('flows').select('*');
-    // if (error) throw error;
+  let account = undefined;
+  try {
+    const accountData = await supabaseClient
+      .from('accounts')
+      .select('*')
+      .filter('user_id', 'eq', user?.id);
+    console.log('🏦 accountData', accountData);
 
-    const { params } = await req.json();
-    console.log('🎛️ params', params);
-    //make sure params.messages is an array and contains at least one message where role and content exist
-    if (
-      !Array.isArray(params.messages) ||
-      params.messages.length === 0 ||
-      !params.messages[0].role ||
-      !params.messages[0].content
-    ) {
-      return new Response(
-        JSON.stringify({
-          error:
-            'params.messages must be an array and contain at least one message where role and content exist',
-        }),
-        {
-          headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-          status: 200,
-        }
-      );
-    }
+    account = accountData?.data?.[0];
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        error: 'Failed to get account from supabase',
+        message: error,
+      }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 400,
+      }
+    );
+  }
+  console.log('💰 account', account);
 
-    const response = await openAi(params); // Call the function
+  // Check if the user has enough credit
+  if (account?.credit < 0.05) {
+    return new Response(
+      JSON.stringify({
+        error: 'You need at least 5c credit to make a call to openAI',
+      }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 200,
+      }
+    );
+  }
 
-    const data = {
-      response,
+  let response = undefined;
+  try {
+    response = await openAi(params);
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 'OpenAI call failed', message: error }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 400,
+      }
+    );
+  }
+  console.log('🤖 response', response);
+
+  const cost = calculateTokenPrice(response.model, response.usage);
+  console.log('🤑 cost', cost);
+  const profitMargin = 2;
+
+  const accountWrite = await supabaseClient
+    .from('accounts')
+    .update({ credit: account?.credit - cost * profitMargin })
+    .match({ user_id: user?.id });
+
+  console.log('🏦 accountWrite', { accountWrite });
+
+  try {
+    const reply = {
+      content: response?.choices[0].message?.content,
+      raw: response,
     };
 
-    return new Response(JSON.stringify(data), {
+    console.log('🚀 Returning data', reply);
+    return new Response(JSON.stringify(reply), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 200,
     });
@@ -115,9 +151,3 @@ serve(async (req) => {
     });
   }
 });
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
