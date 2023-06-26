37ac360f3c664407cca44c432628358db16a9b72
theronburger <dev@theronburger.com>
2023-06-26 19:50:18 +0200

    add in flow editing to text input node

--- libs/ui/src/nodeDialogs/InputDialog/InputDialog.tsx
+++ libs/ui/src/nodeDialogs/InputDialog/InputDialog.tsx
@@ -3,17 +3,45 @@
 import { flowActions, flowSelectors } from "@tool-ai/state";
 import { InnerDialogStructure } from "../../lib/InnerDialogStructure/InnerDialogStructure";
 import { useDispatch, useSelector } from 'react-redux';
+import styles from '../Dialog.module.css';
+import { useState } from "react";
+import Switch from "../../lib/SwitchComponent/SwitchComponent";
+import RadioGroup from "../../lib/RadioGroupComponent/RadioGroupComponent";
 
 function InputDialog({id}:{id:string}) {
   const dispatch = useDispatch();
   const inputs = useSelector(flowSelectors.getInputsById(id));
+  
+  const titleString = inputs?.title as string || 'Text Input';
+
+  const titleMode = inputs?.titleMode as string || 'custom';
+
+
+  const titleModes = [
+    {
+      value:'custom',
+      label:'Custom',
+      description:
+      <p>Displays a user settable string, by default "Preview"</p>
+    },
+    {
+      value:'text-input',
+      label:'Direct',
+      description:<div>
+      <p>Edit the text to be sent directly from the flow</p>
+      </div>
+    }
+  ]
+
+
+  const customStyles = {'--highlight': 'hsla(91, 60%, 66%, 1.0)'}
 
   return (
     <InnerDialogStructure
       title="Text Input"
-      description="text input description"
     >
       <textarea
+        title="Input"
         className="border-2 border-gray-light border-solid rounded-md w-full"
         placeholder="input your text"
         rows={10}
@@ -21,18 +49,81 @@ function InputDialog({id}:{id:string}) {
         value={inputs?.input as string || ''}
         onChange={
           (event) => {
-            console.log('change', event.target.value);
             dispatch(
               flowActions.setInput(
                 {
                   id,
-                  nodeInputs: { input:event.target.value}
+                  nodeInputs: {...inputs, input:event.target.value}
                 }
               )
             )
             }
           }
           />
+                <div title="Options"> 
+        <Switch
+          size="small"
+          label="Editable on node / in place"
+          checked={inputs?.editable as boolean || false}
+          customStyles={customStyles}
+          onCheckedChange={
+            (value) => {
+              dispatch(
+                flowActions.setInput(
+                  {
+                    id,
+                    nodeInputs: {...inputs,  editable:value}
+                  }
+                )
+              )
+              }
+            }
+          />
+        <br/>
+        <RadioGroup
+          title="Flow view title behavior "
+          options={titleModes}
+          value={titleMode}
+          customStyles={customStyles}
+          onChange={
+            (value) => {
+              dispatch(
+                flowActions.setInput(
+                  {
+                    id,
+                    nodeInputs: {...inputs,  titleMode:value, editable:false}
+                  }
+                )
+              )
+              }
+            }
+          size="small"
+          />
+          <br/>
+          { titleMode === 'custom' &&
+            <div>
+              <p><b>String to display</b></p>
+              <input
+              className={styles.TextInput}
+              type="text"
+              value={titleString}
+              placeholder="Text Input"
+              onChange={
+                (event) => {
+                  dispatch(
+                    flowActions.setInput(
+                      {
+                        id,
+                        nodeInputs: {...inputs,  title:event.target.value}
+                      }
+                    )
+                  )
+                  }
+                }
+              />
+            </div>
+          }
+        </div>
     </InnerDialogStructure>
   );
 }
