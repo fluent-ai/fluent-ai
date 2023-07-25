8a650589c9259478697ee2b31599839f62f84de8
theronburger <dev@theronburger.com>
2023-07-25 15:39:07 +0200

    add ws connection management

--- libs/local-runner/src/lib/useLocalRunner.tsx
+++ libs/local-runner/src/lib/useLocalRunner.tsx
@@ -1,15 +1,94 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
-/* eslint-disable-next-line */
-export interface UseLocalRunnerProps {}
+interface WebSocketError extends Error {
+  code?: number;
+  reason?: string;
+  target?: W3CWebSocket;
+  type?: string;
+}
 
-export const useLocalRunner = (props: UseLocalRunnerProps) => {
-    
+export interface UseLocalRunnerProps {
+  host?: string;
+  port?: number;
+  initialReconnectDelay?: number;
+  maxReconnectDelay?: number;
+  retryLimit?: number;
+}
+
+export interface ConnectionState {
+  status: "connected" | "disconnected" | "error";
+  error: WebSocketError | null;
+}
+
+export const useLocalRunner = ({
+  host = '127.0.0.1',
+  port = 8080,
+  initialReconnectDelay = 1000,
+  maxReconnectDelay = 5000,
+  retryLimit = Infinity
+}: UseLocalRunnerProps = {}) => {
+  const url = `ws://${host}:${port}`;
+  const [enabled, setEnabled] = useState(false);
+  const [connectionState, setConnectionState] = useState<ConnectionState>({status: "disconnected", error: null});
+  const [reconnectDelay, setReconnectDelay] = useState(initialReconnectDelay);
+  const [retryCount, setRetryCount] = useState(0);
+  const client = useRef<W3CWebSocket | null>(null);
+
+  const connect = useCallback(() => {
+    setConnectionState({status: "disconnected", error: null});
+    client.current = new W3CWebSocket(url);
+
+    client.current.onopen = () => {
+      setConnectionState({status: "connected", error: null});
+      setRetryCount(0);
+      console.log('🔌 WebSocket Client Connected');
+    };
+
+    client.current.onclose = () => {
+      setConnectionState({status: "disconnected", error: null});
+      if (retryCount < retryLimit) {
+        console.log('🔌 WebSocket Connection Closed. Reconnecting...');
+        setTimeout(connect, reconnectDelay);
+        setReconnectDelay(Math.min(maxReconnectDelay, reconnectDelay + initialReconnectDelay));
+        setRetryCount(retryCount + 1);
+        console.log(`🔌 Reconnect in ${reconnectDelay / 1000}s`);
+      } else {
+        console.log('🔌 Retry Limit Reached. Not Reconnecting');
+      }
+    };
+
+    client.current.onerror = (error: WebSocketError) => {
+      setConnectionState({status: "error", error});
+      console.log('🔌 WebSocket Error: ', error);
+    };
+  }, [url, reconnectDelay, initialReconnectDelay, maxReconnectDelay, retryCount, retryLimit]);
+
+  const disconnect = useCallback(() => {
+    if (client.current) {
+      client.current.close();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (enabled) {
+      connect();
+    } else {
+      disconnect();
+    }
+
+    return disconnect;
+  }, [enabled, connect, disconnect]);
 
   return {
-  }
-  
+    client, 
+    connect,
+    disconnect,
+    setEnabled,
+    enabled,
+    connectionState,
+    retryCount
+  } 
 }
 
 export default useLocalRunner;
