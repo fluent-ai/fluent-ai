92685bc232b90c747d622e8ec196036d56e3961d
theronburger <dev@theronburger.com>
2023-07-25 15:38:02 +0200

    add useLocalRunner

---
+++ libs/local-runner/src/lib/useLocalRunner.tsx
@@ -0,0 +1,15 @@
+import { useRef, useState } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+
+/* eslint-disable-next-line */
+export interface UseLocalRunnerProps {}
+
+export const useLocalRunner = (props: UseLocalRunnerProps) => {
+    
+
+  return {
+  }
+  
+}
+
+export default useLocalRunner;
