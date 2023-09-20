80a92fb6e351beb85885c4b4daef9ac8d4daf5da
theronburger <dev@theronburger.com>
2023-09-20 22:52:02 +0200

    add settings for remote runner

--- libs/ui/src/lib/SettingsDialog/SettingsDialog.tsx
+++ libs/ui/src/lib/SettingsDialog/SettingsDialog.tsx
@@ -1,10 +1,16 @@
 import { DialogComponent } from '../DialogComponent/DialogComponent';
 import { GearIcon } from '@radix-ui/react-icons';
-import Checkbox from '../CheckboxComponent/CheckboxComponent';
+import Switch from '../SwitchComponent/SwitchComponent';
+import { useDispatch, useSelector } from 'react-redux';
+import { generalActions, generalSelectors } from "@tool-ai/state";
+import styles from '../../styles.module.css';
 /* eslint-disable-next-line */
 export interface SettingsDialogProps {}
 
 function SettingsDialog(props: SettingsDialogProps) {
+
+  const dispatch = useDispatch();
+  const remoteRunnerState = useSelector(generalSelectors.getRemoteRunner)
 
   return (
     <DialogComponent
@@ -13,23 +19,34 @@ function SettingsDialog(props: SettingsDialogProps) {
       <div className='sidebar-icon w-[30px] h-[30px]'><GearIcon />
       </div><p className='w-100' aria-label="settings" placeholder="Search nodes" >Settings</p></div>}
     title="Settings"
-    // closeButton={<Form.Submit asChild>
-    //   <ButtonComponent
-    //     type="submit"
-    //     ariaLabel="Save node settings"
-    //     buttonContent="Save"
-    //   />
-    // </Form.Submit>}
     >
       <div>
-      <p>Node Types</p>
-      <Checkbox
-        label="openAI"
-        checked={true}
-        onCheckedChange={(value) => {
-          // supabase.saveSettings({...settings, showOpenAI: value});
-        } }
-        />
+          <p><b>Remote Code Runner</b></p>
+          <p>Enabled</p>
+          <Switch
+          size={'medium'}
+          label={""}
+          checked={remoteRunnerState.enabled}
+          onCheckedChange={(value) => {dispatch(generalActions.setRemoteRunnerEnabled(value))} }
+          />
+          <p>IP Address</p>
+          <input
+            className={styles.TextInput}
+            placeholder={remoteRunnerState.ip}
+            type="text"
+            value={remoteRunnerState.ip}
+            onChange={(event) => dispatch(generalActions.setRemoteRunnerIp(event.target.value))}
+          />
+          <p>Port</p>
+          <input
+            className={styles.TextInput}
+            placeholder={`${remoteRunnerState.port}`}
+            type="number"
+            value={remoteRunnerState.port}
+            onChange={(event) => dispatch(generalActions.setRemoteRunnerPort(Number(event.target.value)))}
+          />
+          <p>Status</p>
+          <p>{remoteRunnerState.status}</p>
         </div>
     
 
