7271faa682a7aeb223b54f0a7d125dd4acfa8d60
theronburger <dev@theronburger.com>
2023-09-20 22:51:45 +0200

    expose general slice

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/State';
 export * from './lib/slices/flow-runner.slice';
 export * from './lib/slices/supabase.slice';
 export * from './lib/slices/flow.slice';
+export * from './lib/slices/general.slice';
