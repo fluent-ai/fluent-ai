ea96cdf4479649d476dae2dc00f9a676f6efb94e
theronburger <dev@theronburger.com>
2023-09-21 19:18:08 +0200

    report supabase errors to avoid CORS in FE

--- apps/remote-runner/functions/readFile.js
+++ apps/remote-runner/functions/readFile.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-function readFile(cwd, { filename }) {
+function readFile(cwd, { filePath }) {
   return new Promise((resolve, reject) => {
-    if (!filename) {
-      reject('expected an object {filename:string}');
+    if (!filePath) {
+      reject('expected an object {filePath:string}');
     }
-    fs.readFile(path.join(cwd, filename), 'utf8', function (error, data) {
+    fs.readFile(path.join(cwd, filePath), 'utf8', function (error, data) {
       if (error) {
         reject(error);
       }
-      console.log(`📖 Read ${filename}`);
+      console.log(`📖 Read ${filePath}`);
       resolve(data);
     });
   });
